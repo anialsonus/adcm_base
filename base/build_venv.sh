27797d857f90072b520158386d8656beff16a9ff
Anton Chevychalov <cab@arenadata.io>
2021-12-08 17:08:19 +0300

    ADCM-2420 create venv with ansible 2.9
    This commmits add /adcm/venv/2.9 dir vith virtual env based on
    https://github.com/arenadata/ansible/commit/c5202a6e92bb578a07e3f42e0580c4c719a8c712

---
+++ base/build_venv.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd /
+
+apply_reqs() {
+    name="/requirements-${1}.txt"
+    pip install --no-cache-dir -r "${name}"
+    rm -f "${name}"
+}
+
+find_venvs(){
+    for i in $(find -name 'requirements-venv-*.txt'); do
+        # Get a venv name with removing of suffix and prefix
+        name="${i%%.txt}"
+        name="${name##./requirements-venv-}"
+        echo "$name"
+    done
+}
+
+apply_reqs "base"
+
+export venv_home="/adcm/venv"
+mkdir -p "${venv_home}"
+
+for v in $(find_venvs); do
+    virtualenv "${venv_home}/${v}"
+    source "${venv_home}/${v}/bin/activate"
+    apply_reqs "venv-${v}"
+done
