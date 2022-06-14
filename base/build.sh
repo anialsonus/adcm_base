776b19757b13e926d01369f1cdd4599be5696123
Alexandr Alferov <aas@arenadata.io>
2022-06-14 12:53:51 +0300

    ADCM-2891 Integration `ldap`

--- base/build.sh
+++ base/build.sh
@@ -5,9 +5,13 @@ cd /
 
 
 apk add --update --no-cache --virtual .pynacl_deps build-base python3-dev libffi-dev openssl-dev linux-headers libxslt-dev rust cargo
+# LDAP
+apk add --update --no-cache openldap-dev
 apk add --update --no-cache libffi openssl libxslt libstdc++ bash
 apk add --update --no-cache nginx sshpass runit openssh-keygen openssh-client git dcron logrotate curl rsync
 apk upgrade --no-cache
+
+ln -s /usr/lib/libldap.so /usr/lib/libldap_r.so
 
 /build_venv.sh
 rm -f /build_venv.sh
